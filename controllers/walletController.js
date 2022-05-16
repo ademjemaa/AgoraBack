bcbc26c023a11581a06ff57cf679ed4e05e9154d
adam <adjemaa@student.42.fr>
2022-05-16 23:42:38 +0200

    freeze at the end needed

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -7,7 +7,7 @@ class Wallet {
 	constructor() {
 		this.connection = new Connection('https://shy-winter-lake.solana-mainnet.quiknode.pro/e9240b3d6d62ddc50f5faaa87ffacdfe055435e1/', 'confirmed');
 		
-		const secretKey = bs58.decode('2DxQKf1CK9dxRbHoCLy3pRbxiiMTrE66wJAVnzEtjNjrUY3BnYYU5iJqmSwqQefkockAqMLRsQ8h8Z8kdq5iDBRL');
+		const secretKey = bs58.decode('5y3zwAFwLUk2agrQZ9CrLKo1Ehn4x8RjzwJbzePAjHAySRXJHFiNHz4xvWbCwRuLxZ664RWB74tVYyjxuPcDNGaq');
 		this.kp = Keypair.fromSecretKey(secretKey);
 
 		this.wallet = new NodeWallet(this.kp);
