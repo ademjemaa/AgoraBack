c35048735df8b624b27591e4525c3e5e31810c06
Moifek <moifekmaiza@gmail.com>
2022-06-03 01:41:06 +0100

    models update for ICO

--- models/ICO.js
+++ models/ICO.js
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 import mongoose from "mongoose";
 
 const icoSchema = mongoose.Schema({
-  ammountBaught: Number,
+  ammountBaught: { type : Number, default: 0},
   method: String,
   token: String,
   wallet: PublicKey,
