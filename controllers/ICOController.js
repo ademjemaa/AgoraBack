fde44c496762cd59acecd3e0cfc74d1eb426d677
ademjemaa <adem.jemaa1998@gmail.com>
2022-06-15 02:40:05 +0000

    latest commit on back

--- controllers/ICOController.js
+++ controllers/ICOController.js
@@ -1,12 +1,18 @@
 import User from "../models/user.js";
 import Ico from "../models/ICO.js";
 import axios from "axios";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+
 import * as web3 from "@solana/web3.js";
 import _stripe from "stripe";
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 import user from "../models/user.js";
 const require = createRequire(import.meta.url); // construct the require method
-const whiteList = require("../config/ICOWhitelist.json");
+const whiteList = require("../config/ICOWhitelist.json")
+const {spawn} = require('child_process');
+const pinataSDK = require('@pinata/sdk');
+const pinata = pinataSDK('dd9892506546216c7b0b', 'ca789c941b9b82210d948d38a611dd79ec69bde59650d08acef0f3974934fcbf');
+
 
 const coins = Number(process.env.TOTALAGORA);
 
@@ -33,12 +39,7 @@ export const BuyIco = async (req, res) => {
   let { amount, paymentMethod, signature } = req.body;
 
   try {
-    await handleICOPurchase({
-      wallet,
-      amount,
-      method: paymentMethod,
-      signature,
-    });
+    await handleICOPurchase({ wallet, amount, method : paymentMethod, signature });
     return res.send("OK");
   } catch (error) {
     console.error(error);
@@ -72,19 +73,70 @@ export const handleStripeCheckout = async (req, res) => {
 };
 
 (async () => {
-  try {
-    // let first = await User.create({
-    // })
-    // first.wallet = "8aZZdRXGxbXuSaU9bgBMyMjKR9g1CM8YpzWUaLF7S4iL";
-    // await first.save();
-    // const result = await User.findOne({wallet: '8aZZdRXGxbXuSaU9bgBMyMjKR9g1CM8YpzWUaLF7S4iL'})
-    // result.icoBaught = 15288 * 1e6;
-    // result.earned = 15288 * 1e6;
-    // await result.delete();
-    // console.log(result);
-  } catch (error) {
-    console.error(error);
+  const account = "CYXxEsNhLHWimv4DVY3KBmYURnewhB8PRu3NKFo3tLjN";
+  let mintPubkey = new web3.PublicKey("CYXxEsNhLHWimv4DVY3KBmYURnewhB8PRu3NKFo3tLjN");
+  let tokenmetaPubkey = await Metadata.getPDA(mintPubkey);
+  let type;
+  let name;
+  let image;
+  const tokenmeta = await Metadata.load(connection, tokenmetaPubkey);
+  if (!tokenmeta.data.data.name.indexOf("Exclusive", 0))
+    return ;
+  if (!tokenmeta.data.data.name.indexOf("Premium", 0))
+  {
+    type = "Exclusive";
+    image = "https://tlbc.mypinata.cloud/ipfs/QmVL85hZGvCXq9C1EfqiW3fJJJp9azyJNR2zEN5iacAZoW";
   }
+  else if (!tokenmeta.data.data.name.indexOf("Standard", 0))
+  {
+    type = "Premium";
+    image = "https://tlbc.mypinata.cloud/ipfs/QmSFnDDPn8B47R3L15iQL5aTpBBJvvEGo4LB4dQwcZEZ79";
+  }
+  name = type + tokenmeta.data.data.name.substring(tokenmeta.data.data.name.indexOf("access", 0) - 1);
+  let number = parseInt(tokenmeta.data.data.name.substring(tokenmeta.data.data.name.indexOf("#", 0) + 1));
+  console.log("edition : " + number);
+  console.log("ipfs image uri : "+image);
+  console.log("card type after upgrade : "+type);
+  const body = {
+    "name":name,
+    "symbol":"ATLBC",
+    "description":"The loft business club is a virtual estate project based on the Solana blockchain offering realistic and customizable flat on the metaverse. To gain access to one of the 5555 Lofts, you need to own an Access Cards. There are three types of them : the Standard (4400 pieces), the Premium (1100 pieces) and the Exclusive (55 pieces). The rarer the Access Card, the bigger the apartment and the amount of special features.",
+    "seller_fee_basis_points":500,
+    "image":image,
+    "external_url":"https://loftsclub.com/",
+    "edition":number,
+    "attributes":[{"trait_type":"access","value":type}],
+    "collection":{"name":"TLBC Access Cards", "family":"Access Cards"},
+    "properties":{"files":[{"type":"image/jpeg","uri":image}],
+    "creators":[{"address":"4KfCr7GQewMMc2xZGz8YSpWJy6PkJdTWhzEAsRboVxe6","share":100}]}
+  
+    };
+    const options = {
+      pinataMetadata: {
+          name: name,
+      },
+      pinataOptions: {
+          cidVersion: 0
+      }
+    };
+    pinata.pinJSONToIPFS(body, options).then((result) => {
+    console.log(result);
+    let dataToSend;
+    const python = spawn('python3', ['/root/metaboss/upgrade.py', account, name, result.IpfsHash]);
+      python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+       });
+       // in close event we are sure that stream from child process is closed
+       python.on('close', (code) => {
+       console.log(`child process close all stdio with code ${code}`);
+       // send data to browser
+       console.log(dataToSend)
+       });
+    
+    }).catch((err) => {
+      console.log(err);
+    });
 })();
 
 const handleICOPurchase = async ({
@@ -108,19 +160,16 @@ const handleICOPurchase = async ({
     const {
       meta: { postTokenBalances, preTokenBalances, postBalances, preBalances },
     } = result;
-
-    amount = Math.abs(
-      ((method === "SOL"
+    
+    amount =
+      Math.abs(((method === "SOL"
         ? postBalances[0]
         : postTokenBalances[0].uiTokenAmount.amount) -
         (method === "SOL"
           ? preBalances[0]
           : preTokenBalances[0].uiTokenAmount.amount)) /
-        1e6
-    );
-    const accounts = result.transaction.message.accountKeys.map((el) =>
-      el.toBase58()
-    );
+      1e6);
+    const accounts = result.transaction.message.accountKeys.map(el => el.toBase58());
     const publicKey = accounts[0];
     if (wallet != publicKey) throw new Error("Wrong public key");
     let sol_price = await getSolanaPrice();
@@ -136,10 +185,12 @@ const handleICOPurchase = async ({
   amount = amount / 0.013;
   console.log("final tokens " + amount);
   let user = await User.findOne({ wallet });
-  if (!user) {
-    user = await User.create({});
+  if (!user)
+  {
+    user = await User.create({
+    })
     user.wallet = wallet;
-  }
+  };
 
   if (amount > (await getRemaingingCoins()))
     throw new Error(
@@ -152,12 +203,13 @@ const handleICOPurchase = async ({
     method,
     signature,
   });
-  console.log("pre buy user" + user);
 
-  if (user.icoBaught == null) {
+  if (user.icoBaught == null)
+  {
     user.icoBaught = 0;
   }
-  if (user.icoBaught != null) {
+  if (user.icoBaught != null)
+  {
     user.icoBaught += _ico.amount * 1e6;
   }
   user.earned += _ico.amount * 1e6;
@@ -201,30 +253,23 @@ export const TotalAgoraLeft = async (req, res) => {
   try {
     return res
       .status(200)
-      .json({
-        left: Math.round((await getRemaingingCoins()) - 20000000),
-        max: coins - 20000000,
-      });
+      .json({ left: Math.round((await getRemaingingCoins()) - 20000000), max: coins - 20000000});
   } catch (error) {
     res.status(401).json({ message: error.message });
   }
 };
 
 export const getPaymentIntent = async (req, res) => {
-  try {
-    const { amount, publicKey } = req.body;
-    if (!publicKey) return res.status(403).send("Public key is required");
+  const { amount, publicKey } = req.body;
+  if (!publicKey) return res.status(403).send("Public key is required");
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
 
-    res.send(paymentIntent.client_secret);
-  } catch (error) {
-    res.status(401).send({ message: error.message });
-  }
+  res.send(paymentIntent.client_secret);
 };
